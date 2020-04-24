f0b4e650dc9ec6ca938e1310a148e132e1f60e7b
JD Veer <veerjd@gmail.com>
2020-04-23 20:29:16 -0400

    Registration rules

--- commands/help.js
+++ commands/help.js
@@ -34,7 +34,7 @@ module.exports = {
         throw `This command doesn't exist.\nGo get some \`${process.env.PREFIX}help\`!`
       else {
         embed.setTitle(`Help card for \`${command.name}\``)
-          .setDescription('Description:', command.description)
+          .setDescription(`Description: ${command.description}`)
           .addField('Usage:', command.usage(process.env.PREFIX))
         if(command.aliases.length > 0)
           embed.addField((command.aliases.length === 1) ? 'Alias:' : 'Aliases', '`' + command.aliases.join('`, `') + '`')
@@ -42,6 +42,7 @@ module.exports = {
       }
     } else {
       const categoriesMapped = {
+        Signup: {},
         Main: {},
         Staff: {}
       }
