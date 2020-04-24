83fe90eb9c0471e8f27ce664ebf6bbe04b66f5be
JD Veer <veerjd@gmail.com>
2020-04-23 20:29:31 -0400

    define and display code

---
+++ commands/code.js
@@ -0,0 +1,37 @@
+const db = require('../db/index')
+const { getUser } = require('../util/utils')
+
+module.exports = {
+  name: 'code',
+  description: 'show code for a user',
+  aliases: [],
+  usage(prefix) {
+    return `\`${prefix}code [alphaseahorse]\``
+  },
+  category: 'Signup',
+  permsAllowed: ['VIEW_CHANNEL'],
+  execute: async function(message, argsStr) {
+
+    const mention = message.mentions.users.first()
+    let user
+    let me = false
+    if(mention)
+      user = getUser(message.guild, mention.username)
+    else if(argsStr)
+      user = getUser(message.guild, argsStr.toLowerCase())
+    else {
+      me = true
+      user = getUser(message.guild, message.author.username)
+    }
+
+    const sql = 'SELECT * FROM codes WHERE player_id = $1'
+    const values = [user.id]
+    const { rows } = await db.query(sql, values)
+
+    if(!rows[0])
+      throw `There doesn't seem to be a code registered with me for ${me ? `you:\nYou can set your own code with \`${process.env.PREFIX}setcode YOURCODE\`` : `**${user.username}**'s code.\nUnfortunately, as of right now, only ${user} can set his own code (with \`${process.env.PREFIX}setcode YOURCODE\`)`}`
+
+    message.channel.send(`Here is **${user.username}**'s code:`)
+    return `\`${rows[0].code}\``
+  }
+};
---
+++ commands/setcode.js
@@ -0,0 +1,42 @@
+const db = require('../db/index')
+
+module.exports = {
+  name: 'setcode',
+  description: 'define your own Polytopia code',
+  aliases: [],
+  usage(prefix) {
+    return `\`${prefix}setcode W8ch0lbiI44Esx6H\``
+  },
+  category: 'Signup',
+  permsAllowed: ['VIEW_CHANNEL'],
+  execute: async function(message, argsStr) {
+
+    const args = argsStr.split(/ +/)
+
+    if(args.length > 1)
+      throw 'Looks like you\'re trying to set someone else\'s code (which I don\'t support)...\nPing them to have them set their own codes!'
+
+    if(args[0].length !== 16)
+      throw 'Why trying to put a random code... :thinking:'
+
+    const user = message.author
+
+    const sqlgc = 'SELECT * FROM codes WHERE player_id = $1'
+    const valuesgc = [user.id]
+    const ressel = await db.query(sqlgc, valuesgc)
+
+    let res
+    if(!ressel.rows[0]) {
+      const sql = 'INSERT INTO codes (player_id, code) VALUES ($1, $2) RETURNING code'
+      const values = [user.id, args[0]]
+      res = await db.query(sql, values)
+    } else {
+      const sql = 'UPDATE codes SET code = $1 WHERE player_id = $2 RETURNING code'
+      const values = [args[0], user.id]
+      res = await db.query(sql, values)
+    }
+
+    message.channel.send(`You just ${!ressel.rows[0] ? 'set' : 'updated'} your code to:`)
+    return `\`${res.rows[0].code}\``
+  }
+};
