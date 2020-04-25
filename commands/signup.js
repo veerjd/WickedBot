dfcb58f3fc1a73b6ffd407de9b399b493f575170
JD Veer <veerjd@gmail.com>
2020-04-25 13:39:52 -0400

    Require setcode for $signup

--- commands/signup.js
+++ commands/signup.js
@@ -1,4 +1,5 @@
 const { getSeasonRole } = require('../util/utils')
+const db = require('../db/index')
 
 module.exports = {
   name: 'signup',
@@ -14,8 +15,15 @@ module.exports = {
 
     const member = message.guild.member(message.author.id)
 
+    const sql = 'SELECT * FROM codes WHERE player_id = $1'
+    const values = [member.user.id]
+    const { rows } = await db.query(sql, values)
+
+    if(rows.length < 1)
+      throw `Make sure you've set your code using \`${process.env.PREFIX}setcode\``
+
     if(member.roles.cache.size < 1)
-      throw 'You need to do a few more practice sets to get familiar with the format and see if you enjoy it enough to commit to a season.\nAfter that, you can request the **@Novice** role to **@Moderator**s.\nMay the odds ever be in your favor!'
+      throw 'You need to do a few more practice sets to get familiar with the format and see if you enjoy it enough to commit to a season.\nAfter that, you can request the @**Novice** role to @**Moderator**s.\nMay the odds ever be in your favor!'
 
     try {
       const seasonRole = await getSeasonRole(message.guild.roles)
