c19fc1e15330e15a8eaedc2ce41e00ff199093f0
JD Veer <veerjd@gmail.com>
2020-04-25 13:52:15 -0400

    Hotfix: perms for Staff cat & signup rules

--- commands/signup.js
+++ commands/signup.js
@@ -19,11 +19,12 @@ module.exports = {
     const values = [member.user.id]
     const { rows } = await db.query(sql, values)
 
+    if(member.roles.cache.size < 2)
+      throw 'You need to do a few more practice sets to get familiar with the format and see if you enjoy it enough to commit to a season.\nAfter that, you can request the @**Novice** role to @**Moderator**s.\nMay the odds ever be in your favor!'
+
     if(rows.length < 1)
       throw `Make sure you've set your code using \`${process.env.PREFIX}setcode\``
 
-    if(member.roles.cache.size < 1)
-      throw 'You need to do a few more practice sets to get familiar with the format and see if you enjoy it enough to commit to a season.\nAfter that, you can request the @**Novice** role to @**Moderator**s.\nMay the odds ever be in your favor!'
 
     try {
       const seasonRole = await getSeasonRole(message.guild.roles)
