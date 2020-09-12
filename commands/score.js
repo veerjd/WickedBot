a4511d66fff6a45112bc57fa0eaee435e62cc1e7
JD Veer <veerjd@gmail.com>
2020-09-12 07:21:44 -0400

    Wrong set id fix

--- commands/score.js
+++ commands/score.js
@@ -59,11 +59,17 @@ module.exports = {
     const sql1Malus = 'SELECT SUM(malus) AS malus, player_id FROM set INNER JOIN points ON set_id = id WHERE season = $1 AND player_id = $2 AND set_id = $3 GROUP BY player_id'
     const values1Malus = [season, player1.id, setId]
     const player1Malus = await db.query(sql1Malus, values1Malus)
+    if(!player1Malus.rows[0])
+      throw 'Are you sure you have the right set ID?'
+
     player1.malus = parseInt(player1Malus.rows[0].malus)
 
     const sql2Malus = 'SELECT SUM(malus) AS malus, player_id FROM set INNER JOIN points ON set_id = id WHERE season = $1 AND player_id = $2 AND set_id = $3 GROUP BY player_id'
     const values2Malus = [season, player2.id, setId]
     const player2Malus = await db.query(sql2Malus, values2Malus)
+    if(!player1Malus.rows[0])
+      throw 'Are you sure you have the right set ID?'
+
     player2.malus = parseInt(player2Malus.rows[0].malus)
 
     player1.pointsWithMalus = player1.points - player1.malus
