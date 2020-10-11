302c842c0cae7d019d7d913d16f842fbc600105f
JD Veer <veerjd@gmail.com>
2020-10-10 23:13:02 -0400

    Merge trailing file

--- commands/season.js
+++ commands/season.js
@@ -27,55 +27,10 @@ module.exports = {
     if(season > currentSeason)
       throw 'I\'m not a wizard, Harry.'
 
-<<<<<<< HEAD
-    const sqlAgg = 'SELECT COUNT(id), SUM(points), SUM(malus) AS malus, player_id FROM set INNER JOIN points ON set_id = id WHERE season = $1 AND completed = true AND is_pro = false GROUP BY player_id HAVING COUNT(id) >= 2'
-    const valuesAgg = [season]
-    const resAgg = await db.query(sqlAgg, valuesAgg)
-    const rowsAgg = resAgg.rows
-    if(rowsAgg.length < 2)
-      throw `Looks like not enough players have enough games (3 needed) for a leaderboard to be generated yet for season ${season}`
-
-    const sql = 'SELECT * FROM set WHERE completed = true AND season = $1 ORDER BY id'
-    const values = [season]
-    const resSets = await db.query(sql, values)
-    const sets = resSets.rows
-
-    const sqlpoints = 'SELECT * FROM points LEFT JOIN set ON set_id = id WHERE completed = true AND season = $1 ORDER BY set_id'
-    const valuespoints = [season]
-    const resPoints = await db.query(sqlpoints, valuespoints)
-    const points = resPoints.rows
-
-    rowsAgg.forEach(player => {
-      const playerPoints = points.filter(x => x.player_id === player.player_id)
-      const playerSets = sets.filter(x => playerPoints.some(y => y.set_id === x.id))
-      const opponentsPoints = points.filter(x => playerSets.some(y => y.id === x.set_id && x.player_id !== player.player_id))
-
-      player.wins = playerPoints.filter(x => x.result === 'win').length
-      player.losses = playerPoints.filter(x => x.result === 'loss').length
-      player.ties = playerPoints.filter(x => x.result === 'tie').length
-
-      let sumOpponent = 0
-      opponentsPoints.forEach(x => {
-        sumOpponent = sumOpponent + x.points - x.malus
-      })
-
-      player.ratio = Number((((parseInt(player.sum) - parseInt(player.malus)) / sumOpponent) * (1.3 - 0.3 * Math.pow(0.85, (player.count - 3)))).toFixed(2))
-    })
-
-    function compare(a, b) {
-      if (a.ratio < b.ratio)
-        return 1;
-      if (a.ratio > b.ratio)
-        return -1;
-      return 0;
-    }
-    rowsAgg.sort(compare)
-=======
     const sqlAgg = 'SELECT * FROM lb WHERE season = $1 ORDER BY rank'
     const valuesAgg = [season]
     const resAgg = await db.query(sqlAgg, valuesAgg)
     const rowsAgg = resAgg.rows
->>>>>>> master
 
     let index = 0
     rowsAgg.forEach(orderedPlayer => {
