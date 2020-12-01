ab49ce09adf7465a4c6512965d1ac048c2f09a4e
JD Veer <veerjd@gmail.com>
2020-11-30 19:54:52 -0500

    Fixed signup

--- commands/signup.js
+++ commands/signup.js
@@ -11,24 +11,23 @@ module.exports = {
   category: 'Basic',
   permsAllowed: ['VIEW_CHANNEL'],
   execute: async function (message) {
-    const sqlseason = 'SELECT season FROM seasons ORDER BY season DESC LIMIT 1'
-    const resSeason = await db.query(sqlseason)
-    const season = resSeason.rows[0].season
-
-    const member = message.guild.member(message.author.id)
-
-    const sql = 'SELECT * FROM codes WHERE player_id = $1 AND'
-    const values = [member.user.id]
-    const { rows } = await db.query(sql, values)
-
-    if (member.roles.cache.size < 2)
-      throw 'You need to do a few more practice sets to get familiar with the format and see if you enjoy it enough to commit to a season.\nAfter that, you can request the @**Novice** role to @**Moderator**s.\nMay the odds ever be in your favor!'
-
-    if (rows.length < 1)
-      throw `Make sure you've set your Polytopia in-game name using \`${process.env.PREFIX}setname\``
-
-
     try {
+      const sqlseason = 'SELECT season FROM seasons ORDER BY season DESC LIMIT 1'
+      const resSeason = await db.query(sqlseason)
+      const season = resSeason.rows[0].season
+
+      const member = message.guild.member(message.author.id)
+
+      const sql = 'SELECT * FROM codes WHERE player_id = $1'
+      const values = [member.user.id]
+      const { rows } = await db.query(sql, values)
+
+      if (member.roles.cache.size < 2)
+        throw 'You need to do a few more practice sets to get familiar with the format and see if you enjoy it enough to commit to a season.\nAfter that, you can request the @**Novice** role to @**Moderator**s.\nMay the odds ever be in your favor!'
+
+      if (rows.length < 1)
+        throw `Make sure you've set your Polytopia in-game name using \`${process.env.PREFIX}setname\``
+
       const seasonRole = await getRegularSeasonRole(message.guild.roles)
       if (member.roles.cache.has(seasonRole.id))
         throw `You are already registered for **${seasonRole.name}**!`
